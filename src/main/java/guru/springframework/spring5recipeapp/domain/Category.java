fedc551a7523074c7d83640355c99164d371b559
Habil Özcan <habil.oezcan@sdx.com>
2021-11-23 13:16:53 +0100

    project lombok is added

--- src/main/java/guru/springframework/spring5recipeapp/domain/Category.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Category.java
@@ -1,38 +1,19 @@
 package guru.springframework.spring5recipeapp.domain;
 
+
+import lombok.*;
 import javax.persistence.*;
 import java.util.Set;
 
+@Data
+@EqualsAndHashCode(exclude = {"recipes"})
 @Entity
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToMany(mappedBy = "categories")
-    private Set<Recipe> recipe;
     private String description;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Set<Recipe> getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Set<Recipe> recipe) {
-        this.recipe = recipe;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        description = description;
-    }
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
 }
